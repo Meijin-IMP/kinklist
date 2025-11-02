958c2cc39e025b158956009b3cba11a7671fbefa
Meijin IMP <39635545+Meijin-IMP@users.noreply.github.com>
2025-11-02 13:51:37 +0100

    IMP temp stage

--- kinklist.js
+++ kinklist.js
@@ -34,23 +34,24 @@ var inputKinks = {}
 var colors = {}
 var level = {};
 
-
+function LoadList() {
+    fileToRead = $("#listType").val() + '.txt';
+    return $.get(fileToRead, function(data) {
+        $('#Kinks').text(data);
+        var selection = inputKinks.saveSelection();
+        var kinksText = $('#Kinks').val();
+        kinks = inputKinks.parseKinksText(kinksText);
+        inputKinks.fillInputList();
+    }, 'text');
+}
 
 $(function(){
 
     var imgurClientId = '9db53e5936cd02f';
 
-    $("#listType").change(function() {
-        fileToRead = $("#listType").val() + '.txt';
-        $.get(fileToRead, function(data) {
-            $('#Kinks').text(data);
-            var selection = inputKinks.saveSelection();
-            var kinksText = $('#Kinks').val();
-            kinks = inputKinks.parseKinksText(kinksText);
-            inputKinks.fillInputList();
-        }, 'text');
+    LoadList();
 
-    }); 
+    $("#listType").change(LoadList()); 
     
     inputKinks = {
         $columns: [],
